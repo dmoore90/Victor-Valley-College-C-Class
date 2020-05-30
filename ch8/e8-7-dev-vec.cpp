da460cbba55020f24f8e9b0a235772e379e54bcc
r34p3r <danieljmoore123@gmail.com>
2020-05-29 17:08:30 -0700

    Updated e8-7

--- ch8/e8-7-dev-vec.cpp
+++ ch8/e8-7-dev-vec.cpp
@@ -24,42 +24,30 @@ int main()
 	// 	in.seekg(-2, in.end);
 	// }
    ifstream in;
-   ofstream out;
     in.open("input.txt");
-    out.open("output.txt");
     char ch;
     int pos;
-    string line, nline, last;
-    int size;
     in.seekg(-1,ios::end);
     pos=in.tellg();
     vector <char> vec_char;
-    for(int i=0;i<=pos;i++)
+    for(int i=0;i<pos;i++)
     {
         ch=in.get();
         // cout<<ch;
-        if (ch == '\n'){
-            int size = line.length() - 1;
-            for (int i = size; i >= 0; i --)
-            {
-                nline += line[i];
-            }
-            out << nline << endl;
 
-            nline = "";
-            line = "";
-        }
-        else if (ch != '\n')
+        if (ch != '\n')
         {
-            line += ch;
+        	vec_char.push_back(ch);
+        }
+        else if (ch == '\n'){
+        	reverse(vec_char.begin(), vec_char.end());
+        	for (int j = 0; j < size(vec_char); j++)
+        	{
+        		cout << vec_char[j];
+        	}
+        	vec_char.clear();
         }
         in.seekg(-2,ios::cur);
     }
-    size = line.length() - 1;
-    for (int i = size; i >= 0; i --)
-    {
-        nline += line[i];
-    }
-    out << nline << endl;
     in.close();
 }
--- ch8/e8-7-dev.cpp
+++ ch8/e8-7-dev.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 
 int main()
 {
-  string line, f;
-  char c;
-  int position;
-  
-  ifstream infile("input.txt");
-  ofstream outfile("output.txt");
-  
-  getline(infile, f);
-  infile.seekg(0, ios::end);
-  
-  int size = infile.tellg();
+   ifstream in;
+   ofstream out;
+    in.open("input.txt");
+    out.open("output.txt");
+    char ch;
+    int pos;
+    string line, nline, last;
+    int size;
+    in.seekg(-1,ios::end);
+    pos=in.tellg();
+    vector <char> vec_char;
+    for(int i=0;i<=pos;i++)
+    {
+        ch=in.get();
+        if (ch != '\n')
+        {
+            line += ch;
+        }
+        else if (ch == '\n'){
+            int size = line.length() - 1;
+            for (int i = size; i >= 0; i --)
+            {
+                nline += line[i];
+            }
+            out << nline << endl;
 
-  while(true)
-  {
-    infile.seekg(-2, ios::cur);
-    if (infile.tellg() < 0) 
-    {
-      break;
+            nline = "";
+            line = "";
+        }
+        in.seekg(-2,ios::cur);
     }
-    infile.get(c);
-    if (c == '\n')
+    size = line.length() - 1;
+    for (int i = size; i >= 0; i --)
     {
-      position = infile.tellg();
-      getline(infile, line);
-      outfile << line << endl;
-      infile.seekg((size - (position - 1)) * -1, ios::end);
+        nline += line[i];
     }
-  }
-  outfile << f << endl;
-  return 0;
+    out << nline << endl;
+    in.close();
 }
